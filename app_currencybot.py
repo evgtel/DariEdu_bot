7ab17c23b04f2d7fd8f7d04603ba380ff3cbc318
evgtel <160165347+evgtel@users.noreply.github.com>
2024-05-07 22:47:18 +0300

    Add files via upload

--- app_currencybot.py
+++ app_currencybot.py
@@ -4,10 +4,15 @@ from extensions import APIException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+    text = 'Привет! Я Бот-конвертер валют.  \nПоказать справку /help'
+    bot.reply_to(message, text)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['help'])
 def handle_start_help(message):
-    bot.reply_to(message, "Для укажите валюту в формате\nВалюта1 Валюта2 Количество\nСписок валют - /values")
+    bot.reply_to(message, "Для конвертации валют\nвведите через пробел:\n "
+                            "<Валюта1> <Валюта2> <Количество>\nСписок валют - /values\nПример:\nдоллар рубль 5")
 
 @bot.message_handler(commands=['values'])
 def handle_values(message):
@@ -17,19 +22,22 @@ def handle_values(message):
 @bot.message_handler()
 def function_name(message):
     try:
-        convert_str = message.text.split(' ')  # ft_key, tt_key, amount
-
+        convert_str=[]
+        pr_str = message.text.split(' ')
+        for ss in pr_str:
+            if ss != '':
+                convert_str.append(ss)
         if len(convert_str) != 3:
             raise APIException("Ошибка: параметров должно быть 3.")
 
-        from_key, to_key, amount = convert_str
-        total_base = CryptoConverter.get_price(from_key, to_key, amount)
+        base, quote, amount = convert_str
+        total_base = CryptoConverter.get_price(base, quote, amount)
     except APIException as e:
         bot.reply_to(message, f'Ошибка пользователя.\n{e}')
     except Exception as e:
         bot.reply_to(message, f'Ошибка программы.\n{e}')
     else:
-        bot.reply_to(message, f"Цена {amount} {from_key} в {to_key} - {total_base}")
+        bot.reply_to(message, f"Цена {amount} {base} - {total_base} {quote}")
 
 
 bot.polling(none_stop=True)
