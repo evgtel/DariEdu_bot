deadeb14dc0672873b623983ff0aec210f6fb08a
Eugen Telelyaev <you@example.com>
2024-08-04 15:37:32 +0300

    Refactor Project

--- app_currencybot.py
+++ app_currencybot.py
@@ -1,12 +1,16 @@
 import telebot
+from telebot import types
 from config import TOKEN, currency
 from extensions import APIException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
+
+
+
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
-    text = 'Привет! Я Бот-конвертер валют.  \nПоказать справку /help'
+    text = 'Hi! Я Бот-конвертер валют.  \nПоказать справку /help'
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['help'])
@@ -18,6 +22,29 @@ def handle_start_help(message):
 def handle_values(message):
     currency_list = '\n'.join(currency)
     bot.reply_to(message, f"Список доступных валют:\n{currency_list}")
+
+
+
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+
+
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text=="Кнопка":
+        fr = message.from_user.id
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Кнопка 2")
+        item2 = types.KeyboardButton("Кнопка 3")
+        markup.add(item1,item2)
+        bot.send_message(message.chat.id,f'User_id:{fr}',reply_markup=markup)
+    elif message.text=="Кнопка 2":
+        bot.send_message(message.chat.id,'Спасибо за прочтение статьи!')
+
 
 @bot.message_handler()
 def function_name(message):
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # t.me/QAP166_bot
 import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 TOKEN = os.getenv('TOKEN')
 #TOKEN = '7025670714:AAGHAXF9fIj8lAMvmY7qhxbhzvsxABIcXPM'
 currency = {'рубль': 'RUB',
